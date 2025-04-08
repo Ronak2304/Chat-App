4a33f53933b7a7ab2e8922de45323b9b959f4844
Ronak Nangia <ronak.23544@sscbs.du.ac.in>
2025-04-08 12:39:44 +0530

    initialized socket server and started with adding real time functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,10 @@ import dotenv from 'dotenv'
 import connectDB from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
+import {app,server} from './lib/socket.js' 
 
 dotenv.config();
 
-const app = express()
 const port = process.env.PORT
 
 app.use(express.json()) // JSON middleware -> extract json data 
@@ -30,7 +29,7 @@ app.use("/api/messages",messageRoutes)
 //     res.send("HELLO WORLD");
 // })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log("Re-Rendering port: "+port);
     connectDB()
 })
